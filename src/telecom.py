28d36330392fc12c6b49d6ef03ae1361e9641184
io <ioflament@gmail.com>
2018-01-13 02:34:35 -0300

    initial cdr eda commit: incomplete

---
+++ src/telecom.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import plotly
+from config import constants, logger_config, _export_dir
+import logging.config
+import logging
+import credentials
+
+plotly.tools.set_credentials_file(username=credentials.plotlyu, api_key=credentials.plotlykey)
+logger = logging.getLogger(__name__)
+
+
+class CDRAnalysis:
+
+    """CDR Analysis"""
+
+    logging.config.dictConfig(logger_config)
+
+    def __init__(self, params, cdr_data):
+        self.params = params
+        self.cdr_foreigners_data = cdr_data.cdr_foreigners_data
+        self.cdr_italians_data = cdr_data.cdr_italians_data
+        self.cdr_main(self.cdr_foreigners_data)
+        self.cdr_main(self.cdr_italians_data)
+
+    # TODO finish EDA function
+    @staticmethod
+    def cdr_main(df):
+
+        """ Exploratory analysis of CDR data """
+
+        # Create a frequency count of how many average  daily calls each customer makes
+        daily_calls = df.groupby(['cust_id', 'date'], as_index=False).count()
+
+        # Create a frequency count of how many average hourly calls each customer makes
+        hourly_calls = df.groupby(['cust_id', 'time'], as_index=False).count()
+
+        # Count calls per customer
+        calls_per_cust = df.groupby(['cust_id'], as_index=False).count()
+
+        # Total estimated daily presences: Italians & Foreigners
+        # Make a stacked bar plot day by day through summer
+
+        # Estimated daily presence of foreign visitors
+
+        # Estimated daily presence of Italian visitors
+
+        # Duration of stay of foreign visitors
+
+        # Duration of stay of Italian visitors
+
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
