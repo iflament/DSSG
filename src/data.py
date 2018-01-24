66122f6f2bdfc7763337222f02c0f51cdfa2f252
io <ioflament@gmail.com>
2018-01-24 09:47:15 -0300

    fixing data module to support click params

--- src/data.py
+++ src/data.py
@@ -13,23 +13,30 @@ class DataHandler:
 
     logging.config.dictConfig(logger_config)
 
-    def __init__(self):
+    def __init__(self, export, sitedata, sitedata_format, cdrdata, cdrdata_format, net_analysis, vis,
+                       sampling, sample_size):
 
-        self.input_params = self._load_params('input_params', constants.INPUT_PARAMS_FILE)
-        logger.info(self.input_params)
-        self.analysis_params = self._load_params('analysis_params', constants.ANALYSIS_PARAMS_FILE)
-        logger.info(self.analysis_params)
+        self.export = export
+        self.sitedata = sitedata
+        self.sitedata_format = sitedata_format
+        self.cdrdata = cdrdata
+        self.cdrdata_format = cdrdata_format
+        self.net_analysis = net_analysis
+        self.vis = vis
+        self.sampling = sampling
+        self.sample_size = sample_size
+        self.click_params, self.params = self._load_params(constants.ANALYSIS_PARAMS_FILE)
 
-        if self.input_params.museum_data:
-            if self.input_params.museum_data_input == 'local_file':
+        if self.sitedata:
+            if self.sitedata_format == 'local_file':
                 self.museum_raw_data = self._get_data_from_file(constants.museum_input_data)
-            elif self.input_params.museum_data_input == 'database':
+            elif self.sitedata_format == 'database':
                 self.museum_raw_data = self._get_data_from_db(constants.museum_input_query)
 
-        if self.input_params.cdr_data:
-            if self.input_params.cdr_data_input == 'local_file':
+        if self.cdrdata:
+            if self.cdrdata_format == 'local_file':
                 self.cdr_raw_data = self._get_data_from_file(constants.cdr_input_data)
-            elif self.input_params.cdr_data_input == 'database':
+            elif self.cdrdata_format == 'database':
                 self.cdr_raw_data = self._get_data_from_db(constants.cdr_input_query)
 
     @staticmethod
@@ -38,12 +45,24 @@ class DataHandler:
 
         return utils.read_yaml(params_file)
 
-    def _load_params(self, name, params_file):
+    def _load_params(self, params_file):
         """ Load analysis params from yaml """
 
         params = self._read_yaml(params_file)
-        d = dict(params)
-        return namedtuple(name, d.keys())(**d)
+        p = dict(params)
+
+        click_p = dict()
+        click_p['export'] = self.export
+        click_p['sitedata'] = self.sitedata
+        click_p['sitedata_format'] = self.sitedata_format
+        click_p['cdrdata'] = self.cdrdata
+        click_p['cdrdata_format'] = self.cdrdata_format
+        click_p['net_analysis'] = self.net_analysis
+        click_p['vis'] = self.vis
+        click_p['sampling'] = self.sampling
+        click_p['sample_size'] = self.sample_size
+
+        return namedtuple('Click_params', click_p.keys())(**click_p), namedtuple('Params', p.keys())(**p)
 
     @staticmethod
     def _get_data_from_db(db_connection, query):
